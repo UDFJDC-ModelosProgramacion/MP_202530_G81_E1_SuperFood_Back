1f9a34c575a7e84ac04131d3aa10f5d8149eded3
Sebastian Acevedo <sracevedob@udistrital.edu.co>
2025-09-22 09:50:40 -0500

    Se añade la RecetaEntity

---
+++ src/main/java/co/edu/udistrital/mdp/back/entities/RecetaEntity.java
@@ -0,0 +1,18 @@
+package co.edu.udistrital.mdp.back.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class RecetaEntity  extends BaseEntity{
+    
+    String nombre;
+    String descripcion;
+    Integer tiempoPreparacion;
+    Integer porciones;
+    
+
+
+}
