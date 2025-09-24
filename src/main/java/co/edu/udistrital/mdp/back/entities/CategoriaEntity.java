44b39672e392ee0e153e97672463346d6f6e42b3
Julian <cajrojasr@udistrital.edu.co>
2025-09-23 21:22:27 -0500

    Se agregan clases nuevas para las entidades evento y categoria

---
+++ src/main/java/co/edu/udistrital/mdp/back/entities/CategoriaEntity.java
@@ -0,0 +1,14 @@
+
+package co.edu.udistrital.mdp.back.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class CategoriaEntity extends BaseEntity {
+
+    private String nombre;        
+    private String descripcion;   
+
+}
