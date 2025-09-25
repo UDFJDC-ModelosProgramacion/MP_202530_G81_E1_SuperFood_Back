11c836b968982c3a4aa21026d4e183bca3781c3b
Julian <cajrojasr@udistrital.edu.co>
2025-09-24 19:21:30 -0500

    Se agregan asociaciones para RestauranteEntity y EventoEntity

--- src/main/java/co/edu/udistrital/mdp/back/entities/EventoEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/EventoEntity.java
@@ -2,16 +2,22 @@
 package co.edu.udistrital.mdp.back.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
-import java.time.LocalDateTime;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class EventoEntity extends BaseEntity {
 
-    private String nombre;          // Nombre del evento
-    private String descripcion;     // Descripción general
-    private String ubicacion;       // Dirección o lugar
-    private LocalDateTime fecha;    // Fecha y hora del evento
+    String fecha; 
 
+    @PodamExclude
+    @OneToOne
+    private ComensalEntity comensal;
+
+    @PodamExclude
+    @OneToOne
+    private ServicioEntity servicio;
 }
+
--- src/main/java/co/edu/udistrital/mdp/back/entities/RestauranteEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/RestauranteEntity.java
@@ -2,7 +2,12 @@
 package co.edu.udistrital.mdp.back.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -14,4 +19,13 @@ public class RestauranteEntity extends BaseEntity {
     private String categoria;
     private Integer porciones;
 
+    
+    @PodamExclude
+    @ManyToOne
+    private Profesional profesional;
+
+    
+    @PodamExclude
+    @OneToMany(mappedBy = "restaurante")
+    private List<FotoEntity> fotos;
 }
