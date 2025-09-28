4a3b81262957fb5cd3d85f161f5ef57873385289
angel4114z <150976042+angel4114z@users.noreply.github.com>
2025-09-28 01:34:13 -0500

    add foto assosiation in PerfilEntity; add UsuarioEntity class with properties and relationships.

--- src/main/java/co/edu/udistrital/mdp/back/entities/PerfilEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/PerfilEntity.java
@@ -1,13 +1,20 @@
 package co.edu.udistrital.mdp.back.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import java.util.ArrayList;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class PerfilEntity extends BaseEntity {
     private String descripcion;
     private String comidaPreferida;
-    private String foto;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "foto")
+    private List<FotoEntity> fotos = new ArrayList<>();
     
 }
