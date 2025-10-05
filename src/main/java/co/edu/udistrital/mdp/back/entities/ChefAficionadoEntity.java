a5f4fc76a2adb3ffb35befcc1df3bec95d38fe42
VoraciusWarden <nihernandezv@udistrital.edu.co>
2025-10-05 12:15:13 -0500

    Business rules, repositories, and services are implemented for ChefAficionado, ChefProfesional, and Utensilios.

--- src/main/java/co/edu/udistrital/mdp/back/entities/ChefAficionadoEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/ChefAficionadoEntity.java
@@ -10,11 +10,28 @@ import java.util.List;
 @Data
 @Entity
 public class ChefAficionadoEntity extends UsuarioEntity {
+
     private String experiencia;
     private String mejorPlatillo;
 
     @PodamExclude
     @OneToMany(mappedBy = "chefAficionado")
-
+    
     private List<RecetaEntity> recetas = new ArrayList<>();
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getMejorPlatillo() {
+        return mejorPlatillo;
+    }
+
+    public void setMejorPlatillo(String mejorPlatillo) {
+        this.mejorPlatillo = mejorPlatillo;
+    }
 }
--- src/main/java/co/edu/udistrital/mdp/back/entities/UtensiliosEntity.java
+++ src/main/java/co/edu/udistrital/mdp/back/entities/UtensiliosEntity.java
@@ -2,21 +2,41 @@ package co.edu.udistrital.mdp.back.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
-import lombok.Data;
-import uk.co.jemos.podam.common.PodamExclude;
 import java.util.ArrayList;
 import java.util.List;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
-@Data
+public class UtensiliosEntity extends BaseEntity {
 
-
-public class UtensiliosEntity extends BaseEntity{
     private String nombre;
-    
+    private String marca;
+
     @PodamExclude
     @ManyToMany(mappedBy = "utensilios")
-
     private List<PreparacionEntity> preparaciones = new ArrayList<>();
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public List<PreparacionEntity> getPreparaciones() {
+        return preparaciones;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setPreparaciones(List<PreparacionEntity> preparaciones) {
+        this.preparaciones = preparaciones;
+    }
 }
