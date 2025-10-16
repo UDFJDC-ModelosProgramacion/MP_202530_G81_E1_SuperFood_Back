c938ddb54cac6c6c9957d0c5f1824a6570e478ae
DIDIEV <stiventorres608@gmail.com>
2025-10-16 09:21:20 -0500

    modified ServicioDTO to add categoriaEnum

--- src/main/java/co/edu/udistrital/mdp/back/dto/ServicioDTO.java
+++ src/main/java/co/edu/udistrital/mdp/back/dto/ServicioDTO.java
@@ -2,10 +2,17 @@ package co.edu.udistrital.mdp.back.dto;
 
 import lombok.Data;
 
+enum CategoriaServicio {
+    CATERING,
+    CHEF_A_DOMICILIO,
+    CLASES_DE_COCINA,
+    CONSULTORIA_GASTRONOMICA
+}
+
 @Data
 public class ServicioDTO {
     String nombre;
     String descripcion;
     double costo;
-    //Enum categoria;
+    CategoriaServicio categoria;
 }
